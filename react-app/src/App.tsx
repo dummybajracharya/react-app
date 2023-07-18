7ee001283d4cfaaf64fefeb1472242c7cfdad090
sanz bajracharya <sanjay.bajracharya4@gmail.com>
2023-07-19 00:11:56 +1000

    Using useState and updating the complex object using spread operators

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,43 @@
-import Message from "./Message";
-import ListGroup from "./components/ListGroup";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({
+    name: "Sanjay",
+    address: "Kogarah",
+    sex: "Male",
+    skills: {
+      csharp: 5,
+      bootstrap: 3,
+    },
+  });
+
+  // Upadate User Address
+  const handleUserClick = () => {
+    console.log("handle clicked");
+    setUser({ ...user, address: "Nepal" });
+  };
+
+  // Update Skills
+  const handleSkillClick = () => {
+    console.log("handleSkillClick clicked");
+    setUser({
+      ...user,
+      skills: {
+        ...user.skills,
+        csharp: 10,
+      },
+    });
+  };
+
   return (
     <>
-      <ListGroup />
+      <h1>
+        {" "}
+        {user.name} - {user.address} - {user.sex}{" "}
+      </h1>
+      <h2> Skills - CSharp: {user.skills.csharp}</h2>
+      <button onClick={handleUserClick}>Update User </button>
+      <button onClick={handleSkillClick}>Update Skill </button>
     </>
   );
 }
